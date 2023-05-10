0e6dd7a2f2edfe9130261ebb7b353b2e41b21037
josema01 <josemanuel.moraga1@alu.uclm.es>
2023-05-10 16:10:24 +0200

    prueba

--- TrabajoPuerto-master/src/MainFrame.java
+++ TrabajoPuerto-master/src/MainFrame.java
@@ -44,7 +44,7 @@ public class MainFrame extends JFrame {
         setVisible(true);
 
 
-        puerto = new Puerto();
+        puerto = new Puerto();//
 
         btnApilar.addActionListener(new ActionListener() {
             @Override
